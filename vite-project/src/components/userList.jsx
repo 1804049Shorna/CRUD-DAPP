492c12bd5ae900c5183f77165a1f15d41b9c3501
Syed Asad Kazmi <sak786@Syeds-MacBook-Air.local>
2024-05-14 06:03:05 +0530

    Deployed new contract and using its ABI

--- vite-project/src/components/userList.jsx
+++ vite-project/src/components/userList.jsx
@@ -1,39 +1,40 @@
 import { useState, useEffect } from "react";
 //import "./UserList.css";
 import "./get.css"
-const userList = ({ state,account }) => {
-    const [user, setUsers] = useState([]);
-   const getuser = async(event)=>{
-   
+const userList = ({ state }) => {
+  const [user, setUsers] = useState([]);
+  const getuser = async (event) => {
+
     event.preventDefault();
     const { contract } = state;
-    const get = await contract.getUser(account)
-       setUsers(get);
-        await get.wait();
-        alert("Fetched successully");
-        window.location.reload();
+    const get = await contract.getUser()
+    console.log(get);
+    setUsers(get);
+    // await get.wait();
+    alert("Fetched successully");
+    // window.location.reload();
 
 
-   }
+  }
 
-  
-           
 
-   return (
+
+
+  return (
     <>
-        <div className="center">
+      <div className="center">
         <p>GET USER </p>
-         <button onClick={getuser}>DELETE</button>
-          {/* <form onSubmit={deleteuser} id="form">
+        <button onClick={getuser}>DELETE</button>
+        {/* <form onSubmit={deleteuser} id="form">
            
             <div className="inputbox">
               <input type="submit" value="DElETE" id="submit" />
             </div>
           </form> */}
-            
-          </div>
+
+      </div>
     </>
-    )
+  )
 };
 
 export default userList;
